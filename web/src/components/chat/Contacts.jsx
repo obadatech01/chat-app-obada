e5ebef9eea52cbd5a6a76150281fee0f636e984d
Obada Yehia <67144995+obadatech01@users.noreply.github.com>
2023-06-19 19:02:21 +0300

    finished page chat 2

--- web/src/components/chat/Contacts.jsx
+++ web/src/components/chat/Contacts.jsx
@@ -1,13 +1,21 @@
 // ch: 11, time: 04:11
 
 import React from 'react'
-import { Row } from 'reactstrap'
+import { Input, Row } from 'reactstrap'
 import Contact from './Contact';
 
 export default class Contacts extends React.Component {
+
+  state = { search: '' }
+
+  onSearch = e => this.setState({search: e.target.value});
+
   render() {
     return (
       <div className='list'>
+        <Row className="search">
+          <Input onChange={this.onSearch} placeholder='بحث' />
+        </Row>
         <Row id="contacts">
           {this.props.contacts.map((contact, index) => this.renderContact(contact, index))}
         </Row>
@@ -16,6 +24,7 @@ export default class Contacts extends React.Component {
   }
 
   renderContact = (contact, index) => {
+    if(!contact.name.includes(this.state.search)) return;
     if(!contact) return;
     // Show only related messages.
     let messages = this.props.messages.filter(e => e.sender === contact.id || e.receiver === contact.id);
